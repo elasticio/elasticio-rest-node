da59913d63247f8953af64fd078b31aa4d72aad5
Igor Drobiazko <igor@elastic.io>
2015-11-16 18:15:59 +0100

    Added POST /tasks to create new tasks: PLT-305

--- spec/resources/tasks.spec.js
+++ spec/resources/tasks.spec.js
@@ -68,4 +68,71 @@ describe('/tasks', function () {
 
         });
     });
+
+    describe('/create', function () {
+
+        it('should send request successfully', function (done) {
+
+            var input = {
+                "name" : "WebHook to Mailchimp",
+                "nodes" : [
+                    {
+                        "action" : "elasticio/webhook:receive",
+                        "config": {
+                            "payload": "email,first,last"
+                        }
+                    },
+                    {
+                        "action" : "elasticio/mapper:map",
+                        "config": {
+                            "mapper" : {
+                                "lastName" : "{{last}}",
+                                "firstName" : "{{first}}",
+                                "salutation" : "{{salutation}}",
+                                "email_type" : "html",
+                                "email" : "{{email}}"
+                            },
+                            "lookupTables": {
+                                "salutation": "lookup-table-id-to-be-used-for-salutation"
+                            }
+                        }
+                    },
+                    {
+                        "action" : "elsaticio/mailchimp:subscribe",
+                        "config": {
+                            "listId" : "8779dd762e",
+                            "_account" : "54536902230d250700000016"
+                        }
+                    }
+                ]
+            };
+
+            var response = {
+                "id": "5602c23e6459bd0500000001",
+                "status": "inactive"
+            };
+
+            nock('https://api.elastic.io')
+                .post('/v1/tasks/', input)
+                .basicAuth({
+                    user: 'root',
+                    pass: 'secret'
+                })
+                .reply(200, response);
+
+            var result;
+
+            tasks
+                .create(input)
+                .then(function (body) {
+                    result = body;
+                })
+                .finally(function () {
+                    expect(result).toEqual(response);
+
+                    done();
+                });
+
+        });
+    });
 });
