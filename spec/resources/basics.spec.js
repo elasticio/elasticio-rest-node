eb84f77e894e9e9cc316c8431e72a34848203c5b
Igor Drobiazko <igor@elastic.io>
2015-11-20 11:05:41 +0100

    Now retrieving default API user, key, uri from env vars

--- spec/resources/basics.spec.js
+++ spec/resources/basics.spec.js
@@ -1,13 +1,98 @@
 describe('Basic use cases', function () {
-    var client = require("../../lib/client")("root", "secret");
-    var users = client.users;
     var nock = require('nock');
 
-    it('should send request successfully', function (done) {
+    afterEach(function (done) {
+        delete process.env.ELASTICIO_API_USERNAME;
+        delete process.env.ELASTICIO_API_KEY;
+        delete process.env.ELASTICIO_API_URI;
+
+        done();
+    });
+
+
+    it('should authenticate with env vars', function (done) {
+        process.env.ELASTICIO_API_USERNAME = '_username_from_env_var';
+        process.env.ELASTICIO_API_KEY = '_apikey_from_env_var';
+
+        var client = require("../../lib/client")();
+
+        var response = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@example.com",
+            "password": "secret",
+            "company": "Doe & Partners"
+        };
+
+        nock('https://api.elastic.io')
+            .get('/v1/users/')
+            .basicAuth({
+                user: '_username_from_env_var',
+                pass: '_apikey_from_env_var'
+            })
+            .reply(200, response);
 
         var result;
 
-        users
+        client
+            .users
+            .me()
+            .then(function (body) {
+                result = body;
+            })
+            .finally(function () {
+                expect(result).toEqual(response);
+
+                done();
+            });
+    });
+
+    it('should use API uri from env vars', function (done) {
+
+        process.env.ELASTICIO_API_URI = 'https://api.elastic-staging-server.com';
+
+        var client = require("../../lib/client")("root", "secret");
+
+
+        var response = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@example.com",
+            "password": "secret",
+            "company": "Doe & Partners"
+        };
+
+        nock('https://api.elastic-staging-server.com')
+            .get('/v1/users/')
+            .basicAuth({
+                user: 'root',
+                pass: 'secret'
+            })
+            .reply(200, response);
+
+        var result;
+
+        client
+            .users
+            .me()
+            .then(function (body) {
+                result = body;
+            })
+            .finally(function () {
+                expect(result).toEqual(response);
+
+                done();
+            });
+
+    });
+
+    it('should send request successfully', function (done) {
+        var client = require("../../lib/client")("root", "secret");
+
+        var result;
+
+        client
+            .users
             .delete()
             .fail(function (e) {
                 result = e;
@@ -22,8 +107,10 @@ describe('Basic use cases', function () {
 
     it('should handle status codes properly', function (done) {
 
+        var client = require("../../lib/client")("root", "secret");
+
         var response = {
-            "error":"Invalid username or secret provided."
+            "error": "Invalid username or secret provided."
         };
 
         nock('https://api.elastic.io')
@@ -33,12 +120,13 @@ describe('Basic use cases', function () {
         var result;
         var error;
 
-        users
+        client
+            .users
             .me()
             .then(function (body) {
                 result = body;
             })
-            .fail(function(e){
+            .fail(function (e) {
                 error = e;
             })
             .finally(function () {
